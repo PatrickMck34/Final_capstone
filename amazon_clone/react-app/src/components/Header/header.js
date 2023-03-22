ef1d681d07d4318044db68a5317f2659b4866f95
Patrick McKinney <106417897+PatrickMck34@users.noreply.github.com>
2023-03-22 08:09:44 -0400

    Dev (#3)
    * sign up page and css
    
    * Fixed Css signup page
    
    * Css on shopping cart and sign up

--- amazon_clone/react-app/src/components/Header/header.js
+++ amazon_clone/react-app/src/components/Header/header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './header.css'
-import { Link} from 'react-router-dom'
+import {  Link} from 'react-router-dom'
+import SignupFormModal from '../SignupFormModal'
+import OpenModalButton from '../OpenModalButton'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
     return(
@@ -18,7 +21,16 @@ function Header() {
             <div className="nav-nav">
                 <div className="nav-option">
                    <span className='nav-option1'>Hello</span>
-                   <span className='nav-option2'>Sign In</span>
+                   <Link to={'/signup'}>
+                   <button className='nav-option2' >Sign In
+                   </button>
+                   </Link>
+                   {/* <OpenModalButton
+                            buttonText={"Sign In"} 
+                            modalComponent={<SignupFormModal/>}/> */}
+                               
+                         
+                         
                 </div>
                 <div className="nav-option">
                  
@@ -30,7 +42,7 @@ function Header() {
                 <i className="fa-solid fa-cart-shopping"  />
                    
                     <span className="nav-option2 basket-count">0</span>
-                    <span></span>
+                   
                 </div>
             </div>
 
--- amazon_clone/react-app/src/components/SignupFormPage/index.js
+++ amazon_clone/react-app/src/components/SignupFormPage/index.js
@@ -12,66 +12,99 @@ function SignupFormPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
-
+  
   if (sessionUser) return <Redirect to="/" />;
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password === confirmPassword) {
-        const data = await dispatch(signUp(username, email, password));
-        if (data) {
-          setErrors(data)
-        }
+      const data = await dispatch(signUp(username, email, password));
+      if (data) {
+        setErrors(data)
+      }
     } else {
-        setErrors(['Confirm Password field must be the same as the Password field']);
+      setErrors(['Confirm Password field must be the same as the Password field']);
     }
   };
-
+  
   return (
     <>
-      <h1>Sign Up</h1>
+    <div className="sign-logo">
+    <img className="signin-logo" src="https://i.postimg.cc/ZYrg8zCH/Simply-Amazin.png"></img>
+    </div>
+
+    <div className="create-container">
+    <div className="signup-logo">
+      <h1>Create Account</h1>
+    </div>
+      <div className="signin-title">
+
+
       <form onSubmit={handleSubmit}>
         <ul>
           {errors.map((error, idx) => <li key={idx}>{error}</li>)}
         </ul>
         <label>
-          Email
+<span className="signin-option">
+        
+   
+
           <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
+          className="sign-options"
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
           />
+         
+          </span>
+          <div className="spacer"></div>
         </label>
         <label>
-          Username
+        <span className="signin-option">
           <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
+          className="sign-options"
+          placeholder="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
           />
+        </span>
+        </label>
+          <div className="spacer"></div>
+        <label>
+        <span className="signin-option">
+          <input
+          className="sign-options"
+          placeHolder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          />
+        </span>
         </label>
         <label>
-          Password
+        <div className="spacer"></div>
+        <span className="signin-option">
           <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
+          className="sign-options"
+          placeholder="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
           />
+          </span>
         </label>
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Sign Up</button>
+        <button className="continue-button" type="submit">Continue</button>
       </form>
+          </div>
+
+
+          </div>
     </>
   );
 }
