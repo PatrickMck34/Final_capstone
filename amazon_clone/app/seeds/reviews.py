2dee4086dce66051bc2b6d895c0c4819be3f3a14
PatrickMck34 <corkscrewhollow@gmail.com>
2023-03-28 11:08:54 -0400

    seeds

--- amazon_clone/app/seeds/reviews.py
+++ amazon_clone/app/seeds/reviews.py
@@ -46,6 +46,6 @@ def seed_reviews():
 def undo_reviews():
     if environment =="production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE")
-    else:
-        db.session.execute(text("DELETE FROM reviews"))
+    # else:
+    #     db.session.execute(text("DELETE FROM reviews"))
     db.session.commit()
