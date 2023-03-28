3f617872344edb8a4439d89d0da347ac0da958b9
PatrickMck34 <corkscrewhollow@gmail.com>
2023-03-28 02:16:21 -0400

    test

--- amazon_clone/react-app/src/components/Home/home.js
+++ amazon_clone/react-app/src/components/Home/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./home.css" 
 import Item from "../Items/Item";
-import { Link } from "react-router-dom";
+
 function Home(){
 
     return (
@@ -10,15 +10,15 @@ function Home(){
                 <img  className="home-banner" src="https://m.media-amazon.com/images/I/719Izgkn1dL._SX3000_.jpg" />
                 <div className="home-line">
 
-                     <Item id={1}title="Amazon Kindle Fire" price={219.99} image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK2xs4CcvDoHUKifWct4HZXwQ7C-129lLBrDXaqqH6P-jqVjOD7lEsA_JjFpLey7DUEQ0&usqp=CAU' alt="loading"
+                     <Item id={1}title="Amazon Kindle Fire" price={219.99} image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK2xs4CcvDoHUKifWct4HZXwQ7C-129lLBrDXaqqH6P-jqVjOD7lEsA_JjFpLey7DUEQ0&usqp=CAU'
                      rating={3}/> 
                    
 
-                     <Item id={2} title="KingTool 325 piece Home Repair Toolkit" price={89.99} image='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSMIzb1QnMIefo_pgAguHcB7mW4-amOfwlGyX9XNnR69V2glGhU' alt="loading"
+                     <Item id={2} title="KingTool 325 piece Home Repair Toolkit" price={89.99} image='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSMIzb1QnMIefo_pgAguHcB7mW4-amOfwlGyX9XNnR69V2glGhU' 
                      rating={5}/>
                      
 
-              <Item id={3} title="ASETY Demascus Knife Set 3PCS, NSF Food-Safe Kitchen Knife Set" price={267.00} image='https://i.postimg.cc/Zn2MLDYp/knife.jpg' alt="loading"
+              <Item id={3} title="ASETY Demascus Knife Set 3PCS, NSF Food-Safe Kitchen Knife Set" price={267.00} image='https://i.postimg.cc/Zn2MLDYp/knife.jpg' 
                  rating={4}/> 
                  
                     {/*  */}
--- amazon_clone/react-app/src/store/item.js
+++ amazon_clone/react-app/src/store/item.js
@@ -5,10 +5,11 @@ const READ_ITEM = '/Items/:itemId'
 
 export const createItem = ({items, itemId}) => async (dispatch) => {
     const {item, rating,} = items
- const data = await fetch(`/api/items/${itemId}` , {
+    const data = await fetch(`/api/items/${itemId}`, {
         method: "POST",
         body: JSON.stringify({
-           item, rating
+           item, 
+           rating
         }),
     });
     const response = await data.json()
