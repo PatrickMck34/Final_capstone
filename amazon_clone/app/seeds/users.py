f9a29afc3ef1037641b008269a83732f167db4e7
PatrickMck34 <corkscrewhollow@gmail.com>
2023-03-29 15:19:00 -0400

    new

--- amazon_clone/app/seeds/users.py
+++ amazon_clone/app/seeds/users.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='1234@44.io', password='password')
+        username='Demdod', email='1234@44.ido', password='padssword')
     # marnie = User(
     #     username='marnie', email='marnie@aa.io', password='password')
     # bobbie = User(
