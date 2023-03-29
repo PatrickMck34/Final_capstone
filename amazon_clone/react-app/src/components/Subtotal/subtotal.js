543ac3ab1e34992c56ea503f690791c3c53e2c00
PatrickMck34 <corkscrewhollow@gmail.com>
2023-03-29 09:54:56 -0400

    rcf

--- amazon_clone/react-app/src/components/Subtotal/subtotal.js
+++ amazon_clone/react-app/src/components/Subtotal/subtotal.js
@@ -4,26 +4,26 @@ import CurrencyFormat from "react-currency-format"
 
 function Subtotal() {
     return( 
-        <div className='subtotal'>
-            <CurrencyFormat
-            renderText={(value) => (
-                <>
+    //     <div className='subtotal'>
+    //         <CurrencyFormat
+    //         renderText={(value) => (
+    //             <>
                 <p>
                     Subtotal (0 items): <strong>0</strong>
                 </p>
-                <small className="subtotal_gift">
-                    <input type="checkbox" /> This order contains a gift
-                </small>
-                </>
-    )}
-            decimalScale={2}
-            value={0}
-            displayType={"text"}
-            thousandSeperator={true}
-            prefix={"$"}
-            />
-            <button>Proceed to Checkout</button>
-        </div>
+    //             <small className="subtotal_gift">
+    //                 <input type="checkbox" /> This order contains a gift
+    //             </small>
+    //             </>
+    // )}
+    //         decimalScale={2}
+    //         value={0}
+    //         displayType={"text"}
+    //         thousandSeperator={true}
+    //         prefix={"$"}
+    //         />
+    //         <button>Proceed to Checkout</button>
+    //     </div>
     )
 }
 export default Subtotal
