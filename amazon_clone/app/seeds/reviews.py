7f7c752bc74bd7ab1c02aec5a927ce944646f407
PatrickMck34 <corkscrewhollow@gmail.com>
2023-03-29 16:34:06 -0400

    seedS

--- amazon_clone/app/seeds/reviews.py
+++ amazon_clone/app/seeds/reviews.py
@@ -8,14 +8,14 @@ def seed_reviews():
     review5 = Review(
         user_id = 10,
         review = "It was ok, money almost",
-        item_id = 3,
+        # item_id = 3,
         user_name = "John"
       
     )
     review6 = Review(
         user_id = 9,
         review = "It was ok, almost",
-        item_id = 3,
+        # item_id = 3,
         user_name = "bob"
       
     )
