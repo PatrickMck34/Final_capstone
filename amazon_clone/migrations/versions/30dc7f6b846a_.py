a693109a13dbcbc2f705ff6d5e6fb6c30f08e90e
PatrickMck34 <corkscrewhollow@gmail.com>
2023-03-29 14:05:22 -0400

    hope

---
+++ amazon_clone/migrations/versions/30dc7f6b846a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 30dc7f6b846a
+Revises: 4a1154dfddba
+Create Date: 2023-03-29 14:04:47.522442
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '30dc7f6b846a'
+down_revision = '4a1154dfddba'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
