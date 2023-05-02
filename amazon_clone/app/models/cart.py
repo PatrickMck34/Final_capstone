1951219e1c0e6c0f27100ca3b55d51c28b07aa0c
PatrickMck34 <corkscrewhollow@gmail.com>
2023-05-02 00:29:34 -0400

    cart Id

--- amazon_clone/app/models/cart.py
+++ amazon_clone/app/models/cart.py
@@ -10,7 +10,7 @@ class Cart(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    # user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     name = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(200))
