24000a2939b555653add8b12d261bbe23f2e314a
PatrickMck34 <corkscrewhollow@gmail.com>
2023-05-03 05:32:18 -0400

    revisions

---
+++ amazon_clone/migrations/versions/28b8b01084cd_.py
@@ -0,0 +1,16 @@
+"""empty message
+
+Revision ID: 28b8b01084cd
+Revises: f2ad61fb8a2b
+Create Date: 2023-03-29 14:16:15.803931
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '28b8b01084cd'
+down_revision = None
+branch_labels = None
+depends_on = None
