e753ee9a94696d50bf57a3b949ec413b259c1e56
PatrickMck34 <corkscrewhollow@gmail.com>
2023-05-09 02:16:08 -0400

    seeder buttons

--- amazon_clone/app/seeds/items.py
+++ amazon_clone/app/seeds/items.py
@@ -2,74 +2,8 @@ from app.models import db, Item, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_items():
-    Computer = Item(
-        name = "Dell XPS 13",
-        price = "$1,199.99",
-        rating = 4.7,
-        imageUrl = "https://m.media-amazon.com/images/I/81akZgRkX9S._AC_SL1500_.jpg",
-        user_id = 2,
-)
-    Router = Item(
-        name = "Netgear Nighthawk X10 AD7200",
-        price = "$449.99",
-        rating = 4.2,
-        imageUrl = "https://m.media-amazon.com/images/I/61qPCEkLwqL._AC_SL1000_.jpg",
-        user_id = 2,
-)
-    Keyboard = Item(
-        name = "Logitech MX Keys",
-        price = "$99.99",
-        rating = 4.6,
-        imageUrl = "https://m.media-amazon.com/images/I/61Ool-DxckL._AC_SL1500_.jpg",
-        user_id = 2,
-)
-    Mouse = Item(
-        name = "Logitech MX Master 3",
-        price = "$99.99",
-        rating = 4.7,
-        imageUrl = "https://m.media-amazon.com/images/I/61SzMhxkzFL._AC_SL1500_.jpg",
-        user_id = 2,
-)
-    External_Hard_Drive = Item(
-        name = "Seagate Backup Plus Slim",
-        price = "$84.99",
-        rating = 4.6,
-        imageUrl = "https://m.media-amazon.com/images/I/81t2WTRQnGL._AC_SL1500_.jpg",
-        user_id = 2,
-)
-    Webcam = Item(
-        name = "Logitech StreamCam",
-        price = "$169.99",
-        rating = 4.5,
-        imageUrl = "https://m.media-amazon.com/images/I/61M28J-ltnL._AC_SL1500_.jpg",
-        user_id = 2,
-)
-    Graphics_Card = Item(
-        name = "Nvidia GeForce RTX 3080",
-        price = "$1,699.99",
-        rating = 4.8,
-        imageUrl = "https://m.media-amazon.com/images/I/81V%2BXZl3%2B-L._AC_SL1500_.jpg",
-        user_id = 2,
-)
-    Wireless_Router = Item(
-        name = "Asus ROG Rapture GT-AX11000",
-        price = "$449.99",
-        rating = 4.4,
-        imageUrl = "https://m.media-amazon.com/images/I/81t1sOOtTIL._AC_SL1500_.jpg",
-        user_id = 2,
-)
 
-
-    items = [
-    Computer,
-    Router,
-    Keyboard,
-    Mouse,
-    External_Hard_Drive,
-    Webcam,
-    Graphics_Card,
-    Wireless_Router,
-]
+    items = []
     add_items = [db.session.add(item) for item in items]
     db.session.commit()
 
--- amazon_clone/react-app/src/components/Items/Item.js
+++ amazon_clone/react-app/src/components/Items/Item.js
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import * as cartActions from '../../store/cart'
 import { useSelector } from "react-redux";
+import * as itemActions from "../../store/item"
 
 function Item({id, title, image, price, rating}) {
     const dispatch = useDispatch()
-    const user = useSelector(state=>state.session.user)
- 
+    
+    const user = useSelector(state=>state.session)
+    const userId = user?.user?.id
     return(
         <div className='Item'>
             <div className="Item-info">
@@ -31,9 +33,9 @@ function Item({id, title, image, price, rating}) {
                     
                      <img src={image}/>
                 
-                        
-                            <button className="Item-button" onClick={(()=>dispatch(cartActions.getCart(user.id)))}>Add to Cart</button>
-                        <Link to={`/review/${title}/${id}`} >
+                     <button className="Item-button" onClick={()=>dispatch(itemActions.createItem4()).then(()=>window.alert("Item added to cart")).then(()=>dispatch(itemActions.getAllItems()))}>Add To Cart</button>
+                     <button className="Item-button" onClick={()=>dispatch(cartActions.createCartItem(userId)).then(()=>window.alert("Item added to Wish List!")).then(()=>dispatch(cartActions.getAllCartItems()))}>Add To List</button>
+                        <Link to={`/review/9`} >
                         <button className="Item-button">See Reviews</button>
                         </Link>
         </div>
